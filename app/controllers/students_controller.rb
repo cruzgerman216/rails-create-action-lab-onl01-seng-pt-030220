fd58194a0353283c438d7348b8cf232a201f7180
German Cruz <cruzgerman216@yahoo.com>
2020-07-08 00:44:50 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new 
+    @student = Student.new
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
     @student.save
